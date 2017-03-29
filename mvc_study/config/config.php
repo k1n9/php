9089d8b314797d11d48892235a2b3f586c159341
k1n9 <wtfk1n9@gmail.com>
2017-03-29 14:32:16 +0800

    创建好基本目录和入口文件

---
+++ mvc_study/config/config.php
@@ -0,0 +1,5 @@
+<?php
+	define('DB_NAME', 'mvc_study');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', 'root');
+	define('DB_HOST', 'localhost');
---
+++ mvc_study/index.php
@@ -0,0 +1,13 @@
+<?php 
+	
+	//应用目录为当前目录
+	define('APP_PATH', __DIR__.'/');
+
+	//开启调试模式
+	define('APP_DEBUG', true);
+
+	//网站根URL
+	define('APP_URL', 'http://hack.lo/mymvc');
+
+	//加载框架
+	require './fastphp/fastphp.php';
